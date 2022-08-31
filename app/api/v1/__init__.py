22e9e918feaf87b09b6d6dc62887c0939dd7f909
NicoRLasso <nicolas.rlasso@gmail.com>
2022-08-31 12:02:15 -0500

    Fix typo on statistics endpoints

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter
 from app.api.v1.endpoints import users_endpoint
 from app.api.v1.endpoints import products_endpoints
 from app.api.v1.endpoints import sales_endpoints
+from app.api.v1.endpoints import statistics_endpoints
+
 
 api_v1_router = APIRouter(prefix="/api/v1")
 
@@ -11,3 +13,5 @@ api_v1_router.include_router(
     products_endpoints.router, prefix="/products", tags=["products"])
 api_v1_router.include_router(
     sales_endpoints.router, prefix="/sales", tags=["sales"])
+api_v1_router.include_router(
+    statistics_endpoints.router, prefix="/statistics", tags=["statistics"])
