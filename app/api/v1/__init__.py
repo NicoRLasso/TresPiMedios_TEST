6d31d8106add9590d2f1fe2c307109a4d1960fe2
NicoRLasso <nicolas.rlasso@gmail.com>
2022-08-30 21:43:27 -0500

    Add products and sales endpoints

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
 from app.api.v1.endpoints import users_endpoint
+from app.api.v1.endpoints import products_endpoints
+from app.api.v1.endpoints import sales_endpoints
 
 api_v1_router = APIRouter(prefix="/api/v1")
 
 api_v1_router.include_router(
     users_endpoint.router, prefix="/users", tags=["users"])
+api_v1_router.include_router(
+    products_endpoints.router, prefix="/products", tags=["products"])
+api_v1_router.include_router(
+    sales_endpoints.router, prefix="/sales", tags=["sales"])
