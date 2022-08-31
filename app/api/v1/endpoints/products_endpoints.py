0100fd9386007418c0a4036bc0543cff22af6e3a
NicoRLasso <nicolas.rlasso@gmail.com>
2022-08-31 10:29:35 -0500

    Add products Auth

--- app/api/v1/endpoints/products_endpoints.py
+++ app/api/v1/endpoints/products_endpoints.py
@@ -1,17 +1,28 @@
+import logging
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Security, HTTPException
+from fastapi.security import APIKeyHeader
 
 from app.services.product_services import ProductServices
 from app.schemas.product_schemas import ProductResponse, ProductSchema
 from app.db.database import db
-
+from app.utils import is_admin_user
 router = APIRouter()
+
+log = logging.getLogger()
 
 
 @router.post("/", response_model=ProductResponse)
-def create_product(new_product: ProductSchema, db_session=Depends(db)):
-    products = ProductServices(db_session)
-    return products.create_product(new_product)
+def create_product(new_product: ProductSchema, db_session=Depends(db), api_key=Security(APIKeyHeader(name="Auth"))):
+    if is_admin_user(api_key, db_session):
+        products = ProductServices(db_session)
+        return products.create_product(new_product)
+    else:
+        log.warning(api_key)
+        raise HTTPException(
+            status_code=401,
+            detail="Unauthorized"
+        )
 
 
 @router.get("/", response_model=List[ProductResponse])
