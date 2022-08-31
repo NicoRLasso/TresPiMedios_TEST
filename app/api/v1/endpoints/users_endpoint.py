c01d3afb7da3d609b8d217cfe52bac32e24607b2
NicoRLasso <nicolas.rlasso@gmail.com>
2022-08-30 21:17:30 -0500

    Add returns type to user logic

--- app/api/v1/endpoints/users_endpoint.py
+++ app/api/v1/endpoints/users_endpoint.py
@@ -1,8 +1,7 @@
+from typing import List
 from uuid import UUID
-from fastapi import APIRouter, Depends, Security
-from starlette import status
+from fastapi import APIRouter, Depends
 
-from app.models.users import Users
 from app.services.user_services import UserServices
 from app.schemas.user_schemas import UserResponse, UserSchema, RoleResponse, RoleSchema
 from app.db.database import db
@@ -28,13 +27,13 @@ def create_roles(new_role: RoleResponse, db_session=Depends(db)):
     return user.create_role(new_role)
 
 
-@router.get("/", response_model=UserResponse)
+@router.get("/", response_model=List[UserResponse])
 def list_users(db_session=Depends(db)):
     user = UserServices(db_session)
     return user.lists_user()
 
 
-@router.get("/roles", response_model=UserResponse)
+@router.get("/roles", response_model=List[UserResponse])
 def list_roles(db_session=Depends(db)):
     user = UserServices(db_session)
     return user.lists_roles()
--- app/services/user_services.py
+++ app/services/user_services.py
@@ -1,42 +1,45 @@
+from typing import List
 from uuid import UUID
 from sqlalchemy.orm import Session
 from app.models.roles import Roles
 
 from app.models.users import Users
 from app.schemas.user_schemas import UserSchema, RoleSchema
+from app.services.app_service import AppService
 
 
-class UserServices:
+class UserServices(AppService):
 
-    def __init__(self, db_session: Session) -> None:
-        self.db_session = db_session
+    def __init__(self, db_session):
+        super().__init__(db_session)
 
-    def create_user(self, new_user_input: UserSchema):
+    def create_user(self, new_user_input: UserSchema) -> Users:
         new_user = Users(
             **new_user_input.dict()
         )
         self.db_session.add(new_user)
         self.db_session.commit()
 
-    def create_role(self, new_role_input: RoleSchema):
+    def create_role(self, new_role_input: RoleSchema) -> Roles:
         new_role = Roles(
             **new_role_input.dict()
         )
         self.db_session.add(new_role)
         self.db_session.commit()
 
-    def assign_role_to_user(self, user_id: UUID, role_id: UUID):
+    def assign_role_to_user(self, user_id: UUID, role_id: UUID) -> Users:
         user = self.db_session.query(Users).filter(Users.id == user_id).first()
         user.role_id = role_id
         self.db_session.commit()
+        return user
 
-    def lists_user(self):
+    def lists_user(self) -> List[Users]:
         return self.db_session.query(Users).all()
 
-    def lists_roles(self):
+    def lists_roles(self) -> List[Roles]:
         return self.db_session.query(Roles).all()
 
-    def delete_user(self, user_id: UUID):
+    def delete_user(self, user_id: UUID) -> str:
         self.db_session.query(Users).filter(Users.id == user_id).delete()
         self.db_session.commit()
         return "Done"
